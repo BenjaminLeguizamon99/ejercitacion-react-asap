407a4e9df683592ca869f917cdadd43d8c706e8b
Benjamin Leguizamon <bleguizamon@asap-consulting.net>
2025-08-28 12:50:03 -0300

    Agregue funcionalidad de editar una tarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import './App.css'
 import Form from './components/Form'
 import { type Task, type Status, type NewTask } from './types'
-
+import { RiDeleteBinLine } from "react-icons/ri";
+import { RiEditFill } from "react-icons/ri";
 
 const estadosPosibles : Status[] = ["Creado", "En curso", "Finalizado"]
 
 function App() {
   const [tareas, setTareas] = useState<Task[]>([])
   const [seleccion, setSeleccion] = useState<Record<number, Status>>({});
+  const [edicionId, setEdicionId] = useState<number | null>(null);
 
+
+  // Cambiar de estado
   const cambiarEstado = (id:number, nuevoEstado:Status) => {
     setTareas(tareas.map(tarea =>
       tarea.id === id ? {...tarea, estado: nuevoEstado} : tarea
     ));
   }
 
+  // Dar de alta tarea
   const onCreate = (newTask:NewTask) => {
     const tareaCompleta: Task = {
       ...newTask,
@@ -26,10 +31,43 @@ function App() {
     setTareas([...tareas, tareaCompleta])
   }
 
+
+  // Eliminar tarea
+  const onDelete = (id:number) => {
+    setTareas(tarea => tarea.filter(t=> t.id !== id));
+  }
+
+  // Editar tarea
+  const editingTask = tareas.find(tarea => tarea.id === edicionId) ?? null;
+
+  const handleUpdate = (id: number, data: NewTask) => {
+    setTareas((prev) => prev.map((t) => (t.id === id? {...t, ...data} : t)))
+    setEdicionId(null);
+  };
+
+  //
+
   return (
     <>
       <h1>Gestor de tareas</h1>
-      <Form onCreate={onCreate} />
+      {editingTask ? (
+        <Form 
+          mode="edit"
+          initialValues={{
+            name : editingTask.name,
+            description : editingTask.description,
+            priority: editingTask.priority,
+            responsable: editingTask.responsable
+          }}
+          onSubmit={(data)=>handleUpdate(editingTask.id, data)}
+          onCancel={()=> setEdicionId(null)}
+          />
+      ): (
+      <Form
+        mode="create"
+        onSubmit={onCreate}
+        />
+      )}
       <div className='card-container'>
       {tareas.map((tarea) => (
         <div key={tarea.id} className='card'>
@@ -44,13 +82,15 @@ function App() {
             }
             >
             {estadosPosibles.map(estado => (
-              <option>{estado}</option>
+              <option key={estado}>{estado}</option>
             ))}
           </select>
           <button  className='btn-estado' onClick={()=>
             cambiarEstado(tarea.id, seleccion[tarea.id]?? tarea.estado)}
             disabled={!seleccion[tarea.id]}
             >Confirmar!</button>
+            <RiDeleteBinLine className='btn-delete' onClick={()=> onDelete(Number(tarea.id))}/>
+            <RiEditFill className='btn-edit' onClick={()=> setEdicionId(tarea.id)} />
         </div>
       ))}
       </div>
